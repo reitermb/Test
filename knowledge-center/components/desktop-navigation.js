dadef857356320ef284922c227a4c7220d4da34a
Miles Reiter <reitermb@gmail.com>
2025-11-13 20:24:01 -0500

    component nav & dynamic current on all pages

--- knowledge-center/components/desktop-navigation.js
+++ knowledge-center/components/desktop-navigation.js
@@ -5,7 +5,19 @@ class SiteNav extends HTMLElement {
       .then(res => res.text())
       .then(html => {
         this.innerHTML = html;
+      // Highlight current page
+        const currentPath = window.location.pathname;
+        const links = this.querySelectorAll('a');
+
+        links.forEach(link => {
+          const linkPath = new URL(link.href).pathname;
+          if (linkPath === currentPath) {
+            link.classList.add('usa-current');
+            link.setAttribute('aria-current', 'page');
+          }
+        });
       })
+
       .catch(err => {
         this.innerHTML = '<p>Error loading navigation.</p>';
         console.error('Nav load error:', err);
